0957824bcc59fe0def24a491ff16d3a58cd5f8ce
singh-aparna <singh123aparna@gmail.com>
2025-01-30 00:21:43 +0530

    Updation

--- src/Webview/Experience/Experience.js
+++ src/Webview/Experience/Experience.js
@@ -6,12 +6,9 @@ const Experience = () => {
     <div className='pl-20 pt-14 flex flex-col gap-y-4  text-white bg-[#3b383f] ' >
       <div className='text-4xl md:text-5xl font-bold text-center tracking-wider'>Experiences</div>
       <ExperienceCard period="Nov,2023-Present" company="DeopersIndia - Noida" profile="Software Engineer"
-        responsibility1="• Completed advanced courses in React.js, Node.js, and Tailwind CSS through Udemy, leveraging these skills to contribute
- effectively to company projects."
-        responsibility2="• Spearheaded the development of key internal projects, such as a UI Clone and Voting App, utilizing both frontend and
- backend technologies to deliver high-quality solutions."
-        responsibility3="• Contributed to open-source repositories as part of company initiatives, improving team collaboration and adhering to best
- practices for clean, maintainable code."
+        responsibility1="• Implemented authentication using JWT & bcrypt.js, handled authentication & protected routes using React Router, and
+ developed APIs using Express.js and Node.js. Integrated real-time tracking with WebSockets to display vehicle status,
+ speed, and geofence alerts efficiently."
       />
       <ExperienceCard period="Mar,2023-October,2023" company="Equipped Analytical Intelligence - Noida" profile="Software Engineer I"
         project="Project - Minerva (Minerva, enables clients to collect, compile, analyse and better understand data in order to help
